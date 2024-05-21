183a32f0c3d0d289e6c5e50f7b3b35972ebfe062
Daniel <danielnavatin@gmail.com>
2024-05-21 16:39:56 +0200

    complete exercise 3.8

--- phonebook-server/index.js
+++ phonebook-server/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+
+morgan.token('post', function (req, res) {
+  console.log(req.method)
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+})
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
 
 let agenda = [
   { 
